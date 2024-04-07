cd74a781b5d3af68b878780add5070060a6f2e8c
ajayXkumar <ajaymatrix386@gmail.com>
2024-04-07 15:53:14 +0530

    about added

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,6 @@
 import React, { useRef, useState } from "react";
 import data from "../data";
 import "./home.css";
-
 const Home = () => {
   const [selectedImage, setSelectedImage] = useState(null);
   const [images, setImages] = useState(data);
@@ -12,26 +11,34 @@ const Home = () => {
 
   return (
     <div className="main-container">
-      <div  className="herosection">
-      <div className="text">
-        <h1 className="head1">CREATIVITY IN PIXELS</h1>
-        <h1 className="head2">GRAPHIC DESIGNE , LOGO DESIGN , WEB DEV</h1>
+      <div className="herosection">
+        <div className="text">
+          <h1 className="head1">CREATIVITY IN PIXELS</h1>
+          <h1 className="head2">GRAPHIC DESIGNE , LOGO DESIGN , WEB DEV</h1>
+        </div>
       </div>
-      </div>
-     
+
       <div className="main-image">
         <img className="image" src="./main.png" alt=""></img>
       </div>
       <div>
-        <p style={{ fontWeight: "bold", fontSize: "30px", textAlign: "center", marginBottom: "2%" }}>DESIGNS</p>
+        <p
+          style={{
+            fontWeight: "bold",
+            fontSize: "30px",
+            textAlign: "center",
+            marginBottom: "2%",
+          }}
+        >
+          DESIGNS
+        </p>
         <div className="all-designs">
           {images.map((item, index) => (
             <div className="text-image">
-            <div className="design-card" key={index}>
-              <img src={item.Image} onClick={() => handleImageClick(item)} />
-              
-            </div>
-            <p>{item.title}</p>
+              <div className="design-card" key={index}>
+                <img src={item.Image} onClick={() => handleImageClick(item)} />
+              </div>
+              <p>{item.title}</p>
             </div>
           ))}
         </div>
@@ -40,20 +47,40 @@ const Home = () => {
         <div className="overlay">
           <div className="overlay-content">
             <div className="big-image-container">
-              <img className="big-image" src={selectedImage.Image} alt={selectedImage.Title} />
+              <img
+                className="big-image"
+                src={selectedImage.Image}
+                alt={selectedImage.Title}
+              />
               <div className="details">
-                <h1 style={{fontWeight:"bold",fontSize:"20px"}}>{selectedImage.title}</h1>
-           
+                <h1 style={{ fontWeight: "bold", fontSize: "20px" }}>
+                  {selectedImage.title}
+                </h1>
               </div>
-              <button className="closebutton" onClick={() => setSelectedImage(null)}>Close</button>
+              <button
+                className="closebutton"
+                onClick={() => setSelectedImage(null)}
+              >
+                Close
+              </button>
             </div>
           </div>
         </div>
       )}
       <div className="footer">
-
-        <p>about section</p>
-      
+        <p style={{fontWeight:"bold" , fontSize:"20px",textAlign:"center",marginBottom:"10px"}}>About Us</p>
+        <p>
+          Welcome to Artiz, where creativity meets excellence! At Artiz, we
+          specialize in crafting captivating designs including posters, mockups,
+          hoodie designs, t-shirt designs, and logos. Our dedicated team ensures
+          high-quality work delivered promptly within 4-5 days, all at a cost
+          that ensures exceptional value. Let us bring your vision to life with
+          our expert design services.
+        </p>
+      </div>
+      <div className="contact">
+        <p style={{fontWeight:"bold" , fontSize:"20px",marginBottom:"20px"}}>Contact Us</p>
+        <a style={{}} href="mailto:ajxvanced@gmail.com">Gmaill</a>
       </div>
     </div>
   );
